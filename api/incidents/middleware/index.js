6643b285e69a3ffd4065a146d1f0ac0bb9dbf58c
Jessica <duellje@oregonstate.edu>
2020-11-13 15:14:07 -0500

    add validator function to make sure all incident keys are present and valid

--- api/incidents/middleware/index.js
+++ api/incidents/middleware/index.js
@@ -1,43 +1,53 @@
-const db = require('../../../data/db-config');
+// const db = require('../../../data/db-config');
 
 module.exports = {
   validateIncidents,
-  validateSource,
 };
 
-function validateIncidents(req, res, next) {
-  if (req.body.length > 0) {
-    req.body = req.body.filter((incident) => {
-      if (
-        incident.lat &&
-        incident.long &&
-        incident.city &&
-        incident.id &&
-        incident.state &&
-        incident.title &&
-        incident.desc &&
-        incident.date
-      ) {
-        const id = db('incidents').where('id', incident.id);
-        if (!id[0]) return incident;
-      }
-    });
-    next();
-  } else {
-    res.status(500).json({ message: 'Error creating Record' });
+function validateIncidents(incident) {
+  const incident_keys = [
+    'title',
+    'desc',
+    'city',
+    'state',
+    'lat',
+    'long',
+    'date',
+    'tags',
+    'src',
+  ];
+  for (let i = 0; i < incident_keys.length; i++) {
+    if (
+      !incident.hasOwnProperty(incident_keys[i]) ||
+      incident[incident_keys[i]] === '' ||
+      incident[incident_keys[i]] === undefined ||
+      incident[incident_keys[i]] === null
+    ) {
+      return 'error';
+    }
   }
+  return 'valid';
 }
 
-function validateSource(req, res, next) {
-  if (req.body != null) {
-    if (req.body.incident_id && req.body.src_url && req.body.src_type) {
-      next();
-    } else {
-      res
-        .status(400)
-        .json({ message: 'Missing incident id, source url or source type' });
-    }
-  } else {
-    res.status(400).json({ message: 'Include source information' });
-  }
-}
+// function validateIncidents(req, res, next) {
+//   if (req.body.length > 0) {
+//     req.body = req.body.filter((incident) => {
+//       if (
+//         incident.lat &&
+//         incident.long &&
+//         incident.city &&
+//         incident.id &&
+//         incident.state &&
+//         incident.title &&
+//         incident.desc &&
+//         incident.date
+//       ) {
+//         const id = db('incidents').where('id', incident.id);
+//         if (!id[0]) return incident;
+//       }
+//     });
+//     next();
+//   } else {
+//     res.status(500).json({ message: 'Error creating Record' });
+//   }
+// }
