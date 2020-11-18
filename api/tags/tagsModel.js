5640eb19383aae24f76271bd0da294c98e51977c
Jessica <duellje@oregonstate.edu>
2020-11-18 16:56:57 -0500

    removed un-needed code from app.js file

--- api/tags/tagsModel.js
+++ api/tags/tagsModel.js
@@ -7,21 +7,26 @@ module.exports = {
   getTagsByName,
 };
 
+//creates new type of force for a given incident
 async function createTags(tag, incidentID) {
+  //checks if the type of force is already in database
   let checkTag = await getTagsByName(tag);
 
   if (checkTag.length <= 0) {
+    //if not in database adds the new type of force to the database and sends the incident id and type of force id to the createIncidentTypeOfForce function
     const tagId = await db('type_of_force').insert(
       { type_of_force: tag },
       'type_of_force_id'
     );
     await createIncidentTypeOfForce(incidentID, tagId[0]);
   } else {
+    //if in database retrieves the id of the type of force and sends the incident id and type of force id to the createIncidentTypeOfForce function
     const forceId = checkTag[0]['type_of_force_id'];
     await createIncidentTypeOfForce(incidentID, forceId);
   }
 }
 
+//adds the relationship between an incident and a type of force into the appropiate table
 async function createIncidentTypeOfForce(incidentID, tagId) {
   await db('incident_type_of_force').insert({
     incident_id: incidentID,
@@ -29,6 +34,7 @@ async function createIncidentTypeOfForce(incidentID, tagId) {
   });
 }
 
+//returns a list of all types of forces in the database
 async function getAllTags() {
   return db('type_of_force as tof')
     .join(
@@ -40,6 +46,7 @@ async function getAllTags() {
     .select('tof.type_of_force_id', 'tof.type_of_force', 'itof.incident_id');
 }
 
+//get all the types of force associated with a given incident id
 async function getTagsByIncidentId(incident_id) {
   return db('type_of_force as tof')
     .join(
@@ -51,6 +58,7 @@ async function getTagsByIncidentId(incident_id) {
     .where('itof.incident_id', incident_id);
 }
 
+//returns the type of force object that matches the type of force given
 async function getTagsByName(type_of_force) {
   return db('type_of_force').where({ type_of_force });
 }
