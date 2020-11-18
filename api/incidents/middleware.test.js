3b0fe3135d1a794261963465ad8a6f96a4ebae0b
Jessica <duellje@oregonstate.edu>
2020-11-18 16:42:42 -0500

    added tests to middleware test file to make it more comprehensive

--- api/incidents/middleware.test.js
+++ api/incidents/middleware.test.js
@@ -89,9 +89,258 @@ describe('processSources', () => {
 });
 
 describe('getStateAbbrev', () => {
-  it('gets state abbreviation', () => {
-    const i1 = incidentsTocheck[0];
-    const abbrev = Middleware.getStateAbbrev(i1.state);
+  it('gets Arizona state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Arizona');
+    expect(abbrev).toBe('AZ');
+  });
+
+  it('gets Alabama state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Alabama');
+    expect(abbrev).toBe('AL');
+  });
+
+  it('gets Alaska state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Alaska');
+    expect(abbrev).toBe('AK');
+  });
+
+  it('gets Arkansas state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Arkansas');
+    expect(abbrev).toBe('AR');
+  });
+
+  it('gets California state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('California');
+    expect(abbrev).toBe('CA');
+  });
+
+  it('gets Colorado state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Colorado');
+    expect(abbrev).toBe('CO');
+  });
+
+  it('gets Connecticut state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Connecticut');
+    expect(abbrev).toBe('CT');
+  });
+
+  it('gets Delaware state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Delaware');
+    expect(abbrev).toBe('DE');
+  });
+
+  it('gets Florida state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Florida');
+    expect(abbrev).toBe('FL');
+  });
+
+  it('gets Georgia state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Georgia');
+    expect(abbrev).toBe('GA');
+  });
+
+  it('gets Hawaii state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Hawaii');
+    expect(abbrev).toBe('HI');
+  });
+
+  it('gets Idaho state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Idaho');
+    expect(abbrev).toBe('ID');
+  });
+
+  it('gets Illinois state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Illinois');
+    expect(abbrev).toBe('IL');
+  });
+
+  it('gets Indiana state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Indiana');
+    expect(abbrev).toBe('IN');
+  });
+
+  it('gets Iowa state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Iowa');
+    expect(abbrev).toBe('IA');
+  });
+
+  it('gets Kansas state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Kansas');
+    expect(abbrev).toBe('KS');
+  });
+
+  it('gets Kentucky state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Kentucky');
+    expect(abbrev).toBe('KY');
+  });
+
+  it('gets Louisiana state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Louisiana');
+    expect(abbrev).toBe('LA');
+  });
+
+  it('gets Maine state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Maine');
+    expect(abbrev).toBe('ME');
+  });
+
+  it('gets Maryland state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Maryland');
+    expect(abbrev).toBe('MD');
+  });
+
+  it('gets Massachusetts state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Massachusetts');
+    expect(abbrev).toBe('MA');
+  });
+
+  it('gets Michigan state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Michigan');
+    expect(abbrev).toBe('MI');
+  });
+
+  it('gets Minnesota state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Minnesota');
+    expect(abbrev).toBe('MN');
+  });
+
+  it('gets Mississippi state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Mississippi');
+    expect(abbrev).toBe('MS');
+  });
+
+  it('gets Missouri state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Missouri');
+    expect(abbrev).toBe('MO');
+  });
+
+  it('gets Montana state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Montana');
+    expect(abbrev).toBe('MT');
+  });
+
+  it('gets Nebraska state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Nebraska');
+    expect(abbrev).toBe('NE');
+  });
+
+  it('gets Nevada state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Nevada');
+    expect(abbrev).toBe('NV');
+  });
+
+  it('gets New Hampshire state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('New Hampshire');
+    expect(abbrev).toBe('NH');
+  });
+
+  it('gets New Jersey state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('New Jersey');
+    expect(abbrev).toBe('NJ');
+  });
+
+  it('gets New Mexico state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('New Mexico');
+    expect(abbrev).toBe('NM');
+  });
+
+  it('gets New York state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('New York');
+    expect(abbrev).toBe('NY');
+  });
+
+  it('gets North Carolina state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('North Carolina');
+    expect(abbrev).toBe('NC');
+  });
+
+  it('gets North Dakota state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('North Dakota');
+    expect(abbrev).toBe('ND');
+  });
+
+  it('gets Ohio state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Ohio');
+    expect(abbrev).toBe('OH');
+  });
+
+  it('gets Oklahoma state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Oklahoma');
+    expect(abbrev).toBe('OK');
+  });
+
+  it('gets Oregon state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Oregon');
     expect(abbrev).toBe('OR');
+  });
+
+  it('gets Pennsylvania state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Pennsylvania');
+    expect(abbrev).toBe('PA');
+  });
+
+  it('gets Rhode Island state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Rhode Island');
+    expect(abbrev).toBe('RI');
+  });
+
+  it('gets South Carolina state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('South Carolina');
+    expect(abbrev).toBe('SC');
+  });
+
+  it('gets South Dakota state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('South Dakota');
+    expect(abbrev).toBe('SD');
+  });
+
+  it('gets Tennessee state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Tennessee');
+    expect(abbrev).toBe('TN');
+  });
+
+  it('gets Texas state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Texas');
+    expect(abbrev).toBe('TX');
+  });
+
+  it('gets Utah state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Utah');
+    expect(abbrev).toBe('UT');
+  });
+
+  it('gets Vermont state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Vermont');
+    expect(abbrev).toBe('VT');
+  });
+
+  it('gets Virginia state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Virginia');
+    expect(abbrev).toBe('VA');
+  });
+
+  it('gets Washington state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Washington');
+    expect(abbrev).toBe('WA');
+  });
+
+  it('gets West Virginia state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('West Virginia');
+    expect(abbrev).toBe('WV');
+  });
+
+  it('gets Wisconsin state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Wisconsin');
+    expect(abbrev).toBe('WI');
+  });
+
+  it('gets Wyoming state abbreviation', () => {
+    const abbrev = Middleware.getStateAbbrev('Wyoming');
+    expect(abbrev).toBe('WY');
+  });
+
+  it('returns false when state does not match the state in the list', () => {
+    const abbrev = Middleware.getStateAbbrev('wyoming');
+    expect(abbrev).toBe(false);
   });
 });
